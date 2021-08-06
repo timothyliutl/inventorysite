be6de8cea446b39a2cfbdc56536f8eafe2155d0b
timothyliutl <18tfl1@queensu.ca>
2021-08-06 14:14:51 -0400

    finished grid and card components

--- src/Components/CardComponent.tsx
+++ src/Components/CardComponent.tsx
@@ -1,29 +1,32 @@
 import { Button, Typography } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import "../Styles/searchStyles.css"
 
 const CardComponent = (props: any) => {
 
+    const [isCardFlipped, flipCard] = useState(false)
 
+    const toggleFlip = () =>{
+        flipCard(!isCardFlipped)
+    }
     return (
         <div className="flip-card">
-            <div className="card-inner">
-
+            <div className={`card-inner ${isCardFlipped?"flip-transform":""}`}>
                 <div className="card-back">
                     <div className="option-box">
                         <div className="container">
-                            <Typography style={{marginTop: '20px'}}>Tutorials</Typography>
-                            <p>Tutorial Videos for Various Labs</p>
-                            <button className="btn return-btn">Go Back</button>
+                            <Typography variant={'h4'} style={{marginTop: '20px'}}>{props.title}</Typography>
+                            <p>{props.description}</p>
+                            <Button onClick={toggleFlip} variant={'contained'} color={'primary'} style={{position:'relative', bottom: '-30px'}}>Return</Button>
                         </div>
                     </div>
                 </div>
                 <div className="card-front">
                     <div className="option-box">
                         <a style={{textDecoration: 'none', color: 'black'}}>
-                            <div className="option-box-image" style={{backgroundImage: 'url(https://asianmedicstore.com/wp-content/uploads/2019/06/support-manual.gif)'}}>
-                                <Typography style={{position:'relative', top:"20px"}} variant={'h4'}>Tutorials</Typography>
-                                <Button variant={'contained'} color={'primary'} style={{position:'relative', bottom:'-90px'}}>More Info</Button>
+                            <div className="option-box-image" style={{backgroundImage: `url(${props.imageurl?props.imageurl:"https://asianmedicstore.com/wp-content/uploads/2019/06/support-manual.gif"})`}}>
+                                <Typography style={{position:'relative', top:"20px"}} variant={'h4'}>{props.title}</Typography>
+                                <Button onClick={toggleFlip} variant={'contained'} color={'primary'} style={{position:'relative', bottom:'-90px'}}>More Info</Button>
                             </div>
                         </a>
                     </div>
--- src/Pages/CategorySearch.tsx
+++ src/Pages/CategorySearch.tsx
@@ -1,10 +1,19 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
 import CardComponent from '../Components/CardComponent';
 
-const CategorySearch = (props:any) =>{
-    return(
+const CategorySearch = (props: any) => {
+    return (
         <div>
-            <CardComponent></CardComponent>
+            <Grid spacing={3} container justifyContent={'center'}>
+                <Grid item>
+                    <CardComponent title={'Tutorials'} description={'Various Tutorial Videos'} imageurl={'https://avatars.githubusercontent.com/u/1965106?s=200&v=4'}></CardComponent>
+                </Grid>
+                <Grid item>
+                    <CardComponent title={'Tutorials'} description={'Various Tutorial Videos'} imageurl={'https://avatars.githubusercontent.com/u/1965106?s=200&v=4'}></CardComponent>
+                </Grid>
+            </Grid>
+
         </div>
     )
 }
